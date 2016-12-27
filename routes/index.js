7cfa67d0e5244e36cb73b31bf993a7dea82aea9c
Jonathan Zong <jonathan@jonathanzong.com>
2016-12-26 23:13:20 -0600

    wildcard get

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('request');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/*', function(req, res, next) {
+  console.log(req.originalUrl);
+  request('http://www.twitter.com' + req.originalUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log(body);
+      // body = '<style>body{transform: rotate(180deg) !important;}</style>' + body;
+      res.send(body);
+    }
+    else {
+      console.err(error);
+    }
+  });
 });
 
 module.exports = router;
