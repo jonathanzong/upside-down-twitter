316d8af34a5fa4282eb99caa3091b09050a29d47
Jonathan Zong <jonathan@jonathanzong.com>
2016-12-26 23:48:29 -0600

    good upside down

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 var request = require('request');
+var cheerio = require('cheerio');
 
-/* GET home page. */
+/* GET */
 router.get('/*', function(req, res, next) {
   console.log(req.originalUrl);
   request('http://www.twitter.com' + req.originalUrl, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      // console.log(body);
-      // body = '<style>body{transform: rotate(180deg) !important;}</style>' + body;
+      $ = cheerio.load(body);
+      var $flip = $('<div class="twitter-flipper"></div>');
+      $('body').wrap($flip);
+      var $style = $('<style>.twitter-flipper{transform: rotate(180deg) !important;}</style>');
+      $('head').append($style);
+      var $scroller = $('<script>$(document).resize(function(){$(document).scrollTop($(document).height());});</script>')
+      res.send($.html());
+    }
+    else {
+      console.error(error);
+    }
+  });
+});
+
+/* POST */
+router.post('/*', function(req, res, next) {
+  console.log(req.originalUrl);
+  request({
+    url: 'http://www.twitter.com' + req.originalUrl,
+    method: 'POST',
+    followAllRedirects: true,
+    headers: req.headers
+    }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
       res.send(body);
     }
     else {
-      console.err(error);
+      console.error(error + response.statusCode);
     }
   });
 });
