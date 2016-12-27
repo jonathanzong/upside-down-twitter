b6c211bec4fecd2f8c829f332baff6182f5a5e07
Jonathan Zong <jonathan@jonathanzong.com>
2016-12-27 01:40:19 -0600

    works except loading new tweets is at the bottom

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,39 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 
+/* GET /i/ */
+router.get('/i/*', function(req, res, next) {
+  console.log(req.originalUrl);
+  request({
+      url: 'http://www.twitter.com' + req.originalUrl,
+      headers: {
+        host: 'twitter.com',
+        referer: 'http://www.twitter.com' + req.originalUrl
+      }
+    }).pipe(res);
+});
+
 /* GET */
 router.get('/*', function(req, res, next) {
   console.log(req.originalUrl);
-  request('http://www.twitter.com' + req.originalUrl, function (error, response, body) {
+
+  request({
+      url: 'http://www.twitter.com' + req.originalUrl
+    }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       $ = cheerio.load(body);
       var $flip = $('<div class="twitter-flipper"></div>');
       $('body').wrap($flip);
       var $style = $('<style>.twitter-flipper{transform: rotate(180deg) !important;}</style>');
       $('head').append($style);
-      var $scroller = $('<script>$(document).resize(function(){$(document).scrollTop($(document).height());});</script>')
+      // var $scroller = $('<script>$(document).scroll(function(){$(document).scrollTop()>=.9*$(document).height()?(console.log("ya dumb fuck"),$(document).scrollTop(.8*$(document).height())):$(document).scrollTop()<=1&&(console.log("gooooo"),$(document).scrollTop($(document).height()),$(document).scrollTop(0))});</script>');
+      // $('body').append($scroller);
       res.send($.html());
     }
     else {
       console.error(error);
+      // console.error(JSON.stringify(response));
+      res.status(response ? response.statusCode : 500).send(error);
     }
   });
 });
@@ -26,19 +44,26 @@ router.get('/*', function(req, res, next) {
 /* POST */
 router.post('/*', function(req, res, next) {
   console.log(req.originalUrl);
-  request({
-    url: 'http://www.twitter.com' + req.originalUrl,
-    method: 'POST',
-    followAllRedirects: true,
-    headers: req.headers
-    }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.send(body);
-    }
-    else {
-      console.error(error + response.statusCode);
-    }
-  });
+  // var headers = JSON.parse(JSON.stringify(req.headers));
+  // headers.host = 'twitter.com';
+  // headers.origin = 'https://twitter.com';
+  // request({
+  //   url: 'https://www.twitter.com' + req.originalUrl,
+  //   method: 'POST',
+  //   followAllRedirects: true,
+  //   headers: headers,
+  //   form: req.body
+  //   }, function (error, response, body) {
+  //   if (!error && response.statusCode == 200) {
+  //     res.send(body);
+  //   }
+  //   else {
+  //     console.error(response.statusCode);
+  //     console.error(JSON.stringify(response));
+  //     res.status(response.statusCode).send(error);
+  //   }
+  // });
+  res.send("");
 });
 
 module.exports = router;
